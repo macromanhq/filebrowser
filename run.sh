efd703e518c7f48afa692b5e59193355aeac0159
Sarim Khan <sarim@macromanhq.com>
2022-05-02 04:26:46 +0600

    rm previous config db on process start to achieve idempotency

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+echo removing previous config
+rm -f /database.db
 /filebrowser config init
-/filebrowser users add $USERNAME $PASSWORD --commands=$ALLOWED_COMMANDS --perm.execute=false --perm.share=false
+/filebrowser users add $USERNAME $PASSWORD --commands=$ALLOWED_COMMANDS --perm.execute=false --perm.share=false --lockPassword
 /filebrowser config set --auth.method=$AUTH_METHOD
 /filebrowser config set --auth.header=$AUTH_HEADER
 /filebrowser
