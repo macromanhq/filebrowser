388b7c73affcfad192935dfb52abcb6fd5258ec3
Sarim Khan <sarim@macromanhq.com>
2022-05-28 06:15:26 +0600

    fix auth proxy header set

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+set -e
+
 echo removing previous config
 rm -f /database.db
 /filebrowser config init
 /filebrowser users add $USERNAME $PASSWORD --commands=$ALLOWED_COMMANDS --perm.execute=false --perm.share=false --lockPassword
-/filebrowser config set --auth.method=$AUTH_METHOD
-/filebrowser config set --auth.header=$AUTH_HEADER
+/filebrowser config set --auth.header=$AUTH_HEADER --auth.method=$AUTH_METHOD
 /filebrowser
