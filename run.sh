e078e0d3275f37626da3b822e29346f77c8a5ded
Sarim Khan <sarim@macromanhq.com>
2022-05-31 23:05:31 +0600

    fix perm.execute

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ set -e
 echo removing previous config
 rm -f /database.db
 /filebrowser config init
-/filebrowser users add $USERNAME $PASSWORD --commands=$ALLOWED_COMMANDS --perm.execute=false --perm.share=false --lockPassword
+[ -z "$ALLOWED_COMMANDS" ] && EXECUTE_PERM=false || EXECUTE_PERM=true
+/filebrowser users add $USERNAME $PASSWORD --commands=$ALLOWED_COMMANDS --perm.execute=$EXECUTE_PERM --perm.share=false --lockPassword
 /filebrowser config set --auth.header=$AUTH_HEADER --auth.method=$AUTH_METHOD
 /filebrowser
